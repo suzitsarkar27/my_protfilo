3632d7fc137b6bf20f5f5c0ce1c541b74c268629
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 13:13:31 +0600

    navbar-completed

---
+++ src/Pages/Bannar/Bannar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Bannar = () => {
+    return (
+        <section>
+            <div className="container f-flex top">
+                 <h3>WELLCOM TO MY WORLD</h3>
+                  <h1>HI I'M <span>Suzit </span></h1>
+            </div>
+        </section>
+    );
+};
+
+export default Bannar;
--- src/Shered/Navbar/Navbar.js
+++ src/Shered/Navbar/Navbar.js
@@ -7,31 +7,32 @@ const Navbar = () => {
         <header>
             <div className="container d_flex">
                 <div className="logo">
-                    <img src="" alt="" />
+                     <h2>SUZIT SARKAR</h2>
                 </div>
 
                 <div className="navlink">
-                    <ul className="link f_flex uppercase">
+                    <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onclick={()=> setMobile(false)}>
                         <li><a href="#home">Home</a></li>
 
                         <li><a href="#features">Features</a></li>
 
                         <li><a href="#portfolio">Portfolio</a></li>
 
-                        <li><a href="#resume"></a>Resume</li>
+                        <li><a href="#resume">Resume</a></li>
 
                         <li><a href="#clients">Clients</a></li>
 
-                        <li><a href="#blog"></a>Blog</li>
+                        <li><a href="#blog">Blog</a></li>
 
                         <li><a href="#contact">Contact</a></li>
 
-                        <li><button className='home_btn'>Buy NOW</button></li>
+                        <li><button className='home-btn'>Buy NOW</button></li>
                     </ul>
 
                     <button className='toggle' onClick={() => setMobile(!Mobile)}>
-                        <i className='fas fa-times close homt-btn'>BUY NOW</i>
-                    </button>
+                        {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+                   </button>
+
                 </div>
             </div>
         </header>
