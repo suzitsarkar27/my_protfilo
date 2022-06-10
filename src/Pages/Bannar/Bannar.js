4320674f89f0123c1d51cee612bae3dfeaf561d8
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 16:07:23 +0600

    bannar-complect

--- src/Pages/Bannar/Bannar.js
+++ src/Pages/Bannar/Bannar.js
@@ -1,64 +1,80 @@
-import React from 'react';
-import { Typewriter } from 'react-simple-typewriter';
-import Skill from '../../assest/pic/skill1.png'
-import Skil2 from '../../assest/pic/skill2.png'
-import Skil3 from '../../assest/pic/skill3.png'
-import hero from '../../assest/pic/hero.png'
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import Skill from "../../assest/pic/skill1.png";
+import Skil2 from "../../assest/pic/skill2.png";
+import Skil3 from "../../assest/pic/skill3.png";
+import hero from "../../assest/pic/hero.png";
+import "./Bannar.css";
 
 const Bannar = () => {
-    return (
-        <section className='hero' id='home'>
-            <div className="container f-flex top">
-                <div className='left top'>
+  return (
+    <>
+      <section className="hero" id="home">
+        <div className="container f_flex top">
+          <div className="left top">
+            <h3>WELCOME TO MY WORLD</h3>
+            <h1>
+              Hi, I’m <span>Suzit</span>
+            </h1>
+            <h2>
+              a
+              <span>
+                <Typewriter
+                  words={[" Professional Coder.", " Developer."]}
+                  loop
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </span>
+            </h2>
 
-                
-                 <h3>WELLCOM TO MY WORLD</h3>
-                  <h1>HI I'M <span>Suzit </span></h1>
-                  <h2>a 
-                      <span>
-                          <Typewriter words={["professional Coder.", "Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000}/>
-                      </span>
-                  </h2>
+            <p>
+              I use animation as a third dimension by which to simplify
+              experiences and kuiding thro each and every interaction. I’m not
+              adding motion just to spruce things up, but doing it in ways that.
+            </p>
 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, quos.</p>
-
-                  <div className="herro_btn d_flex">
-                      <div className="col_1">
-                          <h4>FIND WITH ME</h4>
-                          <div className="button">
-                              <button className='btn_shadow'>
-                                  <i className='fab fa-facebook-f'></i>
-                              </button>
-                              <button className='btn_shadow'>
-                                  <i className='fab fa-instagram'></i>
-                              </button>
-                              <button className='btn_shadow'>
-                                  <i className='fab fa-linkedin-in'></i>
-                              </button>
-                          </div>
-                      </div>
-                      <div className="col_1">
-                          <h4>BEST SKILL NO</h4>
-                          <button className='btn_shadow'>
-                              <img src={Skill} alt="" />
-                          </button>
-                          <button className='btn_shadow'>
-                              <img src={Skil2} alt="" />
-                          </button>
-                          <button className='btn_shadow'>
-                              <img src={Skil3} alt="" />
-                          </button>
-                      </div>
-                  </div>
-             </div>
-             <div className="right">
-                 <div className="right_img">
-                     <img src={hero} alt="" />
-                 </div>
-             </div>
+            <div className="hero_btn d_flex">
+              <div className="col_1">
+                <h4>FIND WITH ME</h4>
+                <div className="button">
+                  <button className="btn_shadow">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>
+                  <button className="btn_shadow">
+                    <i class="fab fa-instagram"></i>
+                  </button>
+                  <button className="btn_shadow">
+                    <i class="fab fa-linkedin-in"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="col_1">
+                <h4>BEST SKILL ON</h4>
+                <button className="btn_shadow">
+                  <img src={Skill} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={Skil2} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={Skil3} alt="" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="right_img">
+              <img src={hero} alt="" />
+            </div>
+          </div>
         </div>
       </section>
-    );
+    </>
+  );
 };
 
 export default Bannar;
--- src/Shered/Navbar/Navbar.js
+++ src/Shered/Navbar/Navbar.js
@@ -2,40 +2,57 @@ import React, { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = () => {
-    const [Mobile, setMobile] = useState(false)
-    return (
-        <header>
-            <div className="container d_flex">
-                <div className="logo">
-                     <h2>SUZIT SARKAR</h2>
-                </div>
+ // fixed Header
+ window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header")
+    header.classList.toggle("active", window.scrollY > 100)
+  })
+  // Toogle Menu
+  const [Mobile, setMobile] = useState(false)
+  return (
+    <>
+      <header className='header'>
+        <div className='container d_flex'>
+          <div className='logo'>
+             <h2>Suzit</h2>
+          </div>
 
-                <div className="navlink">
-                    <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onclick={()=> setMobile(false)}>
-                        <li><a href="#home">Home</a></li>
+          <div className='navlink'>
+            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
+              <li>
+                <a href='#home'>home</a>
+              </li>
+              <li>
+                <a href='#features'>features</a>
+              </li>
+              <li>
+                <a href='#portfolio'>portfolio</a>
+              </li>
+              <li>
+                <a href='#resume'>resume</a>
+              </li>
+              <li>
+                <a href='#clients'>clients</a>
+              </li>
+              <li>
+                <a href='#blog'>blog</a>
+              </li>
+              <li>
+                <a href='#contact'>contact</a>
+              </li>
+              <li>
+                <button className='home-btn'>BUY NOW</button>
+              </li>
+            </ul>
 
-                        <li><a href="#features">Features</a></li>
-
-                        <li><a href="#portfolio">Portfolio</a></li>
-
-                        <li><a href="#resume">Resume</a></li>
-
-                        <li><a href="#clients">Clients</a></li>
-
-                        <li><a href="#blog">Blog</a></li>
-
-                        <li><a href="#contact">Contact</a></li>
-
-                        <li><button className='home-btn'>Buy NOW</button></li>
-                    </ul>
-
-                    <button className='toggle' onClick={() => setMobile(!Mobile)}>
-                        {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
-                   </button>
-
-                </div>
-            </div>
-        </header>
+            <button className='toggle ' onClick={() => setMobile(!Mobile)}>
+              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
     );
 };
 
