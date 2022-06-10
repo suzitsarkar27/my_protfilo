ef9ddb88f2b71f5ec9bec116585e8109661be0ee
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 11:28:08 +0600

    navbar-style

--- src/Shered/Navbar/Navbar.js
+++ src/Shered/Navbar/Navbar.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
+    const [Mobile, setMobile] = useState(false)
     return (
         <header>
             <div className="container d_flex">
@@ -10,17 +12,26 @@ const Navbar = () => {
 
                 <div className="navlink">
                     <ul className="link f_flex uppercase">
-                        <li><a href="#home"></a></li>
-                        <li><a href="#features"></a></li>
-                        <li><a href="#portfolio"></a></li>
-                        <li><a href="#resume"></a></li>
-                        <li><a href="#clients"></a></li>
-                        <li><a href="#blog"></a></li>
-                        <li><a href="#contact"></a></li>
+                        <li><a href="#home">Home</a></li>
+
+                        <li><a href="#features">Features</a></li>
+
+                        <li><a href="#portfolio">Portfolio</a></li>
+
+                        <li><a href="#resume"></a>Resume</li>
+
+                        <li><a href="#clients">Clients</a></li>
+
+                        <li><a href="#blog"></a>Blog</li>
+
+                        <li><a href="#contact">Contact</a></li>
+
                         <li><button className='home_btn'>Buy NOW</button></li>
                     </ul>
 
-                    <button className='toggle'></button>
+                    <button className='toggle' onClick={() => setMobile(!Mobile)}>
+                        <i className='fas fa-times close homt-btn'>BUY NOW</i>
+                    </button>
                 </div>
             </div>
         </header>
