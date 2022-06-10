e2d8209b43711fb99768d0f27e7c603cd36ca28c
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 11:14:44 +0600

    navbar-menu

--- src/Shered/Navbar/Navbar.js
+++ src/Shered/Navbar/Navbar.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is Navbar componont</h1>
-        </div>
+        <header>
+            <div className="container d_flex">
+                <div className="logo">
+                    <img src="" alt="" />
+                </div>
+
+                <div className="navlink">
+                    <ul className="link f_flex uppercase">
+                        <li><a href="#home"></a></li>
+                        <li><a href="#features"></a></li>
+                        <li><a href="#portfolio"></a></li>
+                        <li><a href="#resume"></a></li>
+                        <li><a href="#clients"></a></li>
+                        <li><a href="#blog"></a></li>
+                        <li><a href="#contact"></a></li>
+                        <li><button className='home_btn'>Buy NOW</button></li>
+                    </ul>
+
+                    <button className='toggle'></button>
+                </div>
+            </div>
+        </header>
     );
 };
 
