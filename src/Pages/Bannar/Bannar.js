9c036c0409d29a1182875d7c9ccd5f3ddb4568f5
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 13:58:24 +0600

    bannar-part

--- src/Pages/Bannar/Bannar.js
+++ src/Pages/Bannar/Bannar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
+import Skill from '../../assest/pic/skill1.png'
+import Skil2 from '../../assest/pic/skill2.png'
+import Skil3 from '../../assest/pic/skill3.png'
+import hero from '../../assest/pic/hero.png'
 
 const Bannar = () => {
     return (
-        <section>
+        <section className='hero' id='home'>
             <div className="container f-flex top">
+                <div className='left top'>
+
+                
                  <h3>WELLCOM TO MY WORLD</h3>
                   <h1>HI I'M <span>Suzit </span></h1>
                   <h2>a 
@@ -22,23 +29,35 @@ const Bannar = () => {
                               <button className='btn_shadow'>
                                   <i className='fab fa-facebook-f'></i>
                               </button>
-                              <button>
+                              <button className='btn_shadow'>
                                   <i className='fab fa-instagram'></i>
                               </button>
-                              <button>
+                              <button className='btn_shadow'>
                                   <i className='fab fa-linkedin-in'></i>
                               </button>
                           </div>
                       </div>
                       <div className="col_1">
                           <h4>BEST SKILL NO</h4>
-                          <button>
-                              <img src='' alt="" />
+                          <button className='btn_shadow'>
+                              <img src={Skill} alt="" />
+                          </button>
+                          <button className='btn_shadow'>
+                              <img src={Skil2} alt="" />
+                          </button>
+                          <button className='btn_shadow'>
+                              <img src={Skil3} alt="" />
                           </button>
                       </div>
                   </div>
-            </div>
-        </section>
+             </div>
+             <div className="right">
+                 <div className="right_img">
+                     <img src={hero} alt="" />
+                 </div>
+             </div>
+        </div>
+      </section>
     );
 };
 
