1802e17da2df6e299a91da60c13c7f25af91a66a
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-10 11:02:04 +0600

    y-protfilo

---
+++ src/Shered/Footers/Footers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footers = () => {
+    return (
+        <div>
+            <h2>This is footer compomont</h2>
+        </div>
+    );
+};
+
+export default Footers;
